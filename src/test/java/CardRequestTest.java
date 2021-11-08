de6624bea732d16d4ff914721de3b2da0e2a6b83
Artem Chernyshov <black_art@list.ru>
2021-11-08 22:51:08 +0300

    webdriver added

--- src/test/java/CardRequestTest.java
+++ src/test/java/CardRequestTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriverException;
+
 
 import java.util.List;
 
