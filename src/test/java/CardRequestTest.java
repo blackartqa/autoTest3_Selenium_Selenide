2b314d90a46bd8e0a0f7e79958c51b9d8f7cb662
Artem Chernyshov <black_art@list.ru>
2021-11-08 18:22:26 +0300

    test created

---
+++ src/test/java/CardRequestTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CardRequestTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll(){
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+
+}
