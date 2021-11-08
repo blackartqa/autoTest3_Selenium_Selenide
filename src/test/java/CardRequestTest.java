7cbe7510984e01e2ab8ec51ac584ba6ef6bad8f6
Artem Chernyshov <black_art@list.ru>
2021-11-08 19:38:21 +0300

    test success added

--- src/test/java/CardRequestTest.java
+++ src/test/java/CardRequestTest.java
@@ -1,35 +1,40 @@
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class CardRequestTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setupAll(){
+    static void setupAll() {
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void sholdTestV1(){
+    void sholdTestV1() {
         driver.get("http://localhost:9999/");
-        driver.findElement().sendKeys("Артем");
-        driver.findElement().sendKeys("+75555555555");
-        driver.findElement().click();
-        driver.findElement().click();
-        String actualMessage = driver.findElement().getText();
-        String expectedMessage = "Ваша заявка успешно отправлена";
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Артем Чернышов");
+        textFields.get(1).sendKeys("+75555555555");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actualMessage = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().strip();
+        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expectedMessage, actualMessage, "Фактическое сообщение не соответствет ожмдаемому");
 
     }
