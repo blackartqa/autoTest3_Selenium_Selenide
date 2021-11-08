2c94ff6006f7acca51714a202933fe7aa3080ea2
Artem Chernyshov <black_art@list.ru>
2021-11-08 22:40:16 +0300

    webdriver added

--- src/test/java/CardRequestTest.java
+++ src/test/java/CardRequestTest.java
@@ -9,22 +9,22 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 public class CardRequestTest {
-    private WebDriver driver;
+    public WebDriver driver;
 
     @BeforeAll
     static void setupAll() {
-        //WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
 
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
-//        driver = new ChromeDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -41,7 +41,7 @@ public class CardRequestTest {
         textFields.get(1).sendKeys("+75555555555");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
-        String actualMessage = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().strip();
+        String actualMessage = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().strip();
         String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expectedMessage, actualMessage, "Фактическое сообщение не соответствет ожмдаемому");
 
